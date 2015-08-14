e5ba2f9a9bf3475bce4a247aa6f24309dfcc8dfd
Gunnar Sletta <gunnar@sletta.org>
2015-08-14 14:03:58 +0200

    Updates to CL codes..

--- cpp/opencl/mixed.cpp
+++ cpp/opencl/mixed.cpp
@@ -1,13 +1,24 @@
-#include <OpenCL/opencl.h>
-#include "openclhelpers.h"
 
-#include <GLFW/glfw3.h>
+#include "openclhelpers.h"
+#include "openglhelpers.h"
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void openclErrorCallback(const char *errinfo, const void *privateInfo, size_t cb, void *userData)
+// Globals, put here for convenience...
+static GLFWwindow *window;
+static int windowWidth;
+static int windowHeight;
+static GLuint framebuffer;
+static GLuint framebufferTexture;
+static GLuint fractalProgram;
+static GLuint fractalUniformC;
+static GLuint textureQuadBuffer;
+static GLuint blitProgram;
+
+static void openclErrorCallback(const char *errinfo, const void *privateInfo, size_t cb, void *userData)
 {
     cout << "CL ERROR: '" << errinfo << "'" << endl;
 }
@@ -17,22 +28,7 @@ void glfwErrorCallback(int error, const char *description)
     cout << "GLFW ERROR (" << error << "): " << description << endl;
 }
 
-int main(int argc, char *argv[])
-{
-     // OpenGL setup
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    glfwSetErrorCallback(glfwErrorCallback);
-    cout << "GLFW initialized!" << endl;
-    GLFWwindow *window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    cout << "GLFWwindow: " << window << endl;
-    glfwMakeContextCurrent(window);
-    cout << "OpenGL Context is current!" << endl;
-    cout << "GL_RENDERER ...: " << glGetString(GL_RENDERER) << endl;
-    cout << "GL_VENDOR .....: " << glGetString(GL_VENDOR) << endl;
-    cout << "GL_VERSION ....: " << glGetString(GL_VERSION) << endl;
-    cout << "GL_EXTENSIONS .: " << glGetString(GL_EXTENSIONS) << endl;
-
+static void setup_cl() {
     // OpenCL setup
     cl_device_id devices[4];
     cl_uint deviceCount = 0;
@@ -60,16 +56,145 @@ int main(int argc, char *argv[])
     cout << "OpenCL Context: " << clContext << endl;
     if (error != CL_SUCCESS) {
         cout << "Epic failure when creating OpenCL Context....: " << error << " (0x" << hex << error << ")" << endl;
-        return 0;
+        return;
     }
+}
 
-    int frameCounter = 1000;
-    while (!glfwWindowShouldClose(window) && --frameCounter > 0)
+static void initialize_opengl()
+{
+     // OpenGL setup
+    if (!glfwInit()) {
+        cerr << "glfwInit: failed!!" << endl;       
+        exit(1);
+    }
+    glfwSetErrorCallback(glfwErrorCallback);
+    cout << "GLFW initialized!" << endl;
+    window = glfwCreateWindow(1280, 720, "Mixing OpenGL & OpenCL", NULL, NULL);
+    glfwGetWindowSize(window, &windowWidth, &windowHeight);
+    cout << "GLFWwindow ....: " << window << "; size=" << windowWidth << "," << windowHeight << endl;
+    glfwMakeContextCurrent(window);
+    cout << "OpenGL Context is current!" << endl;
+    cout << "GL_RENDERER ...: " << glGetString(GL_RENDERER) << endl;
+    cout << "GL_VENDOR .....: " << glGetString(GL_VENDOR) << endl;
+    cout << "GL_VERSION ....: " << glGetString(GL_VERSION) << endl;
+    cout << "GL_EXTENSIONS .: " << glGetString(GL_EXTENSIONS) << endl;
+    cout << endl;
+
+    glewExperimental=GL_TRUE;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        cerr << "glewInit: failed!" << glewGetErrorString(err) << endl;
+        exit(1);       
+    }
+    cout << "GLEW initialized!" << endl << endl;
+
+    cout << "OpenGL Objects:" << endl;
+    framebuffer = gl_create_framebufferobject(windowWidth, windowHeight, &framebufferTexture);
+    cout << " - framebuffer .......: " << framebuffer 
+         << " (texture=" << framebufferTexture << ")" << endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    const char *fractalAttributes[] = { "aV", "aTC", 0 };
+    fractalProgram = gl_create_program(// Vertex Shader
+                                      "\n attribute vec4 aV;"
+                                      "\n attribute vec2 aTC;"
+                                      "\n varying vec2 vTC;"
+                                      "\n void main() {"
+                                      "\n     gl_Position = aV;"
+                                      "\n     vTC = 3.0 * (aTC - 0.5);"
+                                      "\n }",
+                                      // Fragment Shader
+                                      "\n uniform vec2 c;"
+                                      "\n varying vec2 vTC;"
+                                      "\n #define ITERATIONS 50"
+                                      "\n void main() {"
+                                      "\n      vec2 z = vTC;"
+                                      "\n     int i;"
+                                      "\n     for (i=0; i<ITERATIONS; ++i) {"
+                                      "\n         float x = (z.x * z.x - z.y * z.y) + c.x;"
+                                      "\n         float y = (z.y * z.x + z.x * z.y) + c.y;"
+                                      "\n         if (x*x + y*y > 4.0) break;"
+                                      "\n         z = vec2(x,y);"
+                                      "\n     }"
+                                      "\n     float v = i == ITERATIONS ? 0.0 : pow(float(i) / float(ITERATIONS), 0.5);"
+                                      "\n     gl_FragColor = vec4(v * vec3(v, v*v, 1), 1);"
+                                      "\n }", 
+                                      fractalAttributes);
+    cout << " - fractal shader ....: " << fractalProgram << endl;
+
+    const char *blitAttributes[] = { "aV", "aTC", 0 };
+    blitProgram = gl_create_program(// Vertex Shader
+                                    "\n attribute vec2 aV;"
+                                    "\n attribute vec2 aTC;"
+                                    "\n varying vec2 vTC;"
+                                    "\n void main() {"
+                                    "\n     gl_Position = vec4(aV, 0, 1);"
+                                    "\n     vTC = aTC;"
+                                    "\n }",
+                                    // Fragment Shader
+                                    "\n uniform sampler2D T;"
+                                    "\n varying vec2 vTC;"
+                                    "\n void main() {"
+                                    "\n     gl_FragColor = texture2D(T, vTC);"
+                                    "\n }", 
+                                    blitAttributes);
+    cout << " - blit shader .......: " << blitProgram << endl;
+
+    glGenBuffers(1, &textureQuadBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, textureQuadBuffer);
+    float data[] = { -1, -1, 0, 0, 
+                      1, -1, 1, 0,
+                     -1,  1, 0, 1,
+                      1,  1, 1, 1 };
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+    cout << " - vertex buffer .....: " << textureQuadBuffer << endl;
+
+}
+
+static void renderToFramebuffer()
+{
+    glViewport(0, 0, windowWidth, windowHeight);
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+
+
+    // Render the fractal to the FBO
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glUseProgram(fractalProgram);
+    static double t = 0; 
+    t += 0.01;
+    float cx = sin(t) * 0.5;
+    float cy = cos(t);
+    glUniform2f(fractalUniformC, cx, cy);
+    glBindBuffer(GL_ARRAY_BUFFER, textureQuadBuffer);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void *) (sizeof(float) * 2));
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // Optionally, render the FBO to the screen...
+    glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+    glUseProgram(blitProgram);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glUseProgram(0);
+}
+
+int main(int argc, char *argv[])
+{
+    initialize_opengl();
+
+    while (!glfwWindowShouldClose(window))
     {
-        float color = frameCounter % 2;
-        glClearColor(color, 0, 1.0 - color, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
-
+        renderToFramebuffer();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -82,6 +207,6 @@ int main(int argc, char *argv[])
 // Building...
 //
 // OSX: clang++ -framework OpenCL -framework OpenGL -I/usr/local/include mixed.cpp -L/usr/local/lib -lglfw3  -o mixed && ./mixed
-//
+// Linux: g++ mixed.cpp -lOpenCL -lglfw -lGL -o mixed
 //
 
---
+++ cpp/opencl/openglhelpers.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cassert>
+#include <cstring>
+
+inline GLuint gl_create_shader(const char *sh, GLenum type)
+{
+    GLuint id = glCreateShader(type);
+    int len = std::strlen(sh);
+    glShaderSource(id, 1, &sh, &len);
+    glCompileShader(id);
+    int param = 0;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &param);
+    if (param == GL_FALSE) {
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &param);
+        char *str = (char *) malloc(param + 1);
+        int l = 0;
+        glGetShaderInfoLog(id, param, &l, str);
+        assert(l < param);
+        str[l] = '\0';
+        std::cout << "error: Failed to compile shader: " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl
+             << sh << std::endl
+             << "error: " << str << std::endl;
+        free(str);
+        assert(false);
+    }
+    return id;
+}
+
+inline GLuint gl_create_program(const char *vsh, const char *fsh, const char *attr[]) 
+{
+    GLuint vid = gl_create_shader(vsh, GL_VERTEX_SHADER);
+    assert(vid);
+
+    GLuint fid = gl_create_shader(fsh, GL_FRAGMENT_SHADER);
+    assert(fid);
+
+    GLuint pid = glCreateProgram();
+    assert(pid);
+
+    glAttachShader(pid, vid);
+    glAttachShader(pid, fid);
+    for (unsigned i=0; attr[i]; ++i)       
+        glBindAttribLocation(pid, i, attr[i]);
+    glLinkProgram(pid);
+
+    int param = 0;
+    glGetProgramiv(pid, GL_LINK_STATUS, &param);
+    if (param == GL_FALSE) {
+        glGetProgramiv(pid, GL_INFO_LOG_LENGTH, &param);
+        char *str = (char *) malloc(param + 1);
+        int l = 0;
+        glGetProgramInfoLog(pid, param, &l, str);
+        assert(l < param);
+        str[l] = '\0';
+        std::cerr << "error: Failed to link shader program:" << std::endl
+                  << "Vertex Shader:" << std::endl << vsh << std::endl
+                  << "FragmentShader:" << std::endl << fsh << std::endl
+                  << "error: " << str << std::endl;
+        free(str);
+        assert(false);
+    }
+
+    assert(glGetError() == GL_NO_ERROR);
+    return pid;    
+}
+
+
+inline GLuint gl_create_texture(int w, int h, void *data = 0)
+{
+	GLuint tex;
+	glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    return tex;
+}
+
+inline GLuint gl_create_framebufferobject(int width, int height, GLuint *texture) 
+{
+    assert(texture);
+    *texture = gl_create_texture(width, height);
+	GLuint fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, *texture, 0);
+
+#ifndef NDEBUG
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        std::cerr << "Failed! gl_create_framebufferobject(" << width << ", " << height 
+        		  << ") texture=" << texture << std::endl;
+        assert(false);
+    }
+#endif
+
+    return fbo;
+}
